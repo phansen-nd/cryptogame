5b03fc518394344733a0632e79c44e0742d04a7a
Pat Hansen <patrick.hansen1618@gmail.com>
2022-12-23 13:56:16 -0800

    Add time delays

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
+from time import sleep
+
 XOR_KEY = '10101010'
 
+def sleep_for(seconds):
+    for i in range(seconds):
+        print(str(seconds - i) + '...')
+        sleep(1)
 
 def ordinals(message):
+    sleep_for(15)
     return ''.join([str(ord(c)) + " " for c in message])
 
 def caesar(message):
+    sleep_for(30)
     return ''.join([chr(ord(c) + 3) + " " for c in message])
 
 def binary(message):
+    sleep_for(60)
     return ''.join([bin(ord(c))[2:].zfill(8) + " " for c in message])
 
 def xor(message):
+    sleep_for(120)
     return ''.join([chr(int(bin(ord(c))[2:].zfill(8), 2) ^ int(XOR_KEY, 2)) + " " for c in message])
 
 
